1d309bad66a5387c32ed2d5769fb0732dc37adf1
Jeewon Kim <gabyjennn@gmail.com>
2023-04-18 11:19:39 -0400

    drum update

--- drumKit.js
+++ drumKit.js
@@ -27,37 +27,47 @@ function makeSound(key) {
 
   switch (key) {
     case "w":
-      var tom1 = new Audio("sounds/clap.wav");
+      var clap = new Audio("sounds/clap.wav");
       tom1.play();
       break;
 
     case "a":
-      var tom2 = new Audio("sounds/hihat.wav");
+      var hihat = new Audio("sounds/hihat.wav");
       tom2.play();
       break;
 
     case "s":
-      var tom3 = new Audio('sounds/boom.wav');
+      var boom = new Audio('sounds/boom.wav');
       tom3.play();
       break;
 
     case "d":
-      var tom4 = new Audio('sounds/snare.wav');
+      var snare = new Audio('sounds/snare.wav');
+      tom4.play();
+      break;
+
+    case "f":
+      var openhat = new Audio('sounds/openhat.wav');
+      tom4.play();
+      break;
+
+    case "g":
+      var ride = new Audio('sounds/ride.wav');
       tom4.play();
       break;
 
     case "j":
-      var snare = new Audio('sounds/kick.wav');
+      var kick = new Audio('sounds/kick.wav');
       snare.play();
       break;
 
     case "k":
-      var crash = new Audio('sounds/tink.wav');
+      var tink = new Audio('sounds/tink.wav');
       crash.play();
       break;
 
     case "l":
-      var kick = new Audio('sounds/tom.wav');
+      var tom = new Audio('sounds/tom.wav');
       kick.play();
       break;
 
